e4457cd8a0c31283346b593de23fee6af6b31501
hlthomas <hthomas16@fordham.edu>
2025-02-13 17:20:12 -0500

    committing in class assignment 9 and text

---
+++ InClassAssignments/in-class-9/in-class-9.js
@@ -0,0 +1,15 @@
+const hasPinecone = (str) => {
+    return str.toLowerCase().includes('pinecone');
+};
+
+const sentences = [
+    "How are pinecone created?",
+    "I love the purple skys",
+    "Collecting pinecones is my hobby",
+    "Cats are the best thing created",
+    "Do pinecones taste like sap?"
+];
+
+const pineconeResults = sentences.filter(hasPinecone);
+
+console.log("Sentences containing 'pinecone':", pineconeResults);
