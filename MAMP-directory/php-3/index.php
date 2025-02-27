a23f8c7285af8e1da36684287d146caa6a39ffab
hlthomas <hthomas16@fordham.edu>
2025-02-27 17:00:38 -0500

    committing in class 13

---
+++ MAMP-directory/php-3/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP App</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+<h1><?php echo $required1; ?></h1>
+<button id="getDataButton">Click Me</button>
+<div id="result"></div>
+
+<script>
+    $(document).ready(function() {
+        $('#getDataButton').click(function() {
+            $.get('getData.php', function(data) {
+                $('#result').text(data.message);
+            });
+        });
+    });
+</script>
+</body>
+</html>
